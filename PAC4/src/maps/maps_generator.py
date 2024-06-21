83c102cbdd019f5e1cccd6fec7105baec2b72a2d
Albrath <aloarter@gmail.com>
2024-06-21 18:01:31 +0200

    PAC4 - Ejercicio 6, generacion de mapas

--- PAC4/src/maps/maps_generator.py
+++ PAC4/src/maps/maps_generator.py
@@ -1,6 +1,17 @@
 from ..maps import maps_generator_operations as m
+from pandas import DataFrame
+import os
 
 
-def prepare_and_create_maps(firearms_df):
+def prepare_and_create_maps(firearms_df: DataFrame) -> None:
+    """
+    Prepare the directory and create the maps for the data in the dataframe
+    :param firearms_df:     dataframe with firearms data
+    """
+    print(f"\n6.1")
     state_geo = "https://raw.githubusercontent.com/python-visualization/folium/main/examples/data/us-states.json"
-    m.create_maps(firearms_df, state_geo)
+    # Creo el directorio para depositar los mapas generados
+    if not os.path.exists("../generated"):
+        os.makedirs("../generated")
+    # Creo los mapas
+    m.create_maps(state_geo, firearms_df)
--- PAC4/src/maps/maps_generator_operations.py
+++ PAC4/src/maps/maps_generator_operations.py
@@ -1,31 +1,47 @@
 import folium
+import io
 from pandas import DataFrame
+from PIL import Image
 
 
-def create_maps(firearms_df: DataFrame, state_geo_url: str) -> None:
+def create_maps(state_geo_url: str, firearms_df: DataFrame) -> None:
+    """
+    Creates maps trough folium library using the data from the firearms dataframe and the state geography
+    :param state_geo_url:   url where the states geographic json data is located
+    :param firearms_df:     dataframe with the data extracted from exercise 5.5
+    :return:
+    """
     create_map(
         state_geo_url,
         firearms_df,
         ["code", "permit_perc"],
         "Firearms permit percentage",
-        "firearms_permit_percentage_map.html")
+        "m1")
 
     create_map(
         state_geo_url,
         firearms_df,
         ["code", "longgun_perc"],
         "Firearms long gun percentage",
-        "firearms_longgun_percentage_map.html")
+        "m2")
 
     create_map(
         state_geo_url,
         firearms_df,
         ["code", "handgun_perc"],
         "Firearms hand gun percentage",
-        "firearms_handgun_percentage_map.html")
+        "m3")
 
 
-def create_map(state_geo_url: str, firearms_df: DataFrame, columns: list, title: str, map_name: str):
+def create_map(state_geo_url: str, firearms_df: DataFrame, columns: list, title: str, map_name: str) -> None:
+    """
+    Creates a map from the dataframe from the given columns
+    :param state_geo_url:   Url where the states info json data is located
+    :param firearms_df:     dataframe with the data extracted from exercise 5.5
+    :param columns:         columns to be drawn in the map
+    :param title:           title of the map that will apear in the legend
+    :param map_name:        file name of the map
+    """
     firearm_map = folium.Map(location=[40, -95], zoom_start=4)
     folium.Choropleth(
         geo_data=state_geo_url,
@@ -38,6 +54,22 @@ def create_map(state_geo_url: str, firearms_df: DataFrame, columns: list, title:
         line_opacity=.1,
         legend_name=title,
     ).add_to(firearm_map)
-
     folium.LayerControl().add_to(firearm_map)
-    firearm_map.save(map_name)
+    save_map(firearm_map, title, map_name)
+
+
+def save_map(firearm_map: folium.Map, title: str, map_name: str) -> None:
+    """
+    Save the previously created map from the folium library into html and png
+    :param firearm_map:     folium map to be saved as files
+    :param title:           title of the map that will apear in the legend
+    :param map_name:        file name of the map
+    :return:
+    """
+    maps_path = "../generated/"
+    firearm_map.save(f"{maps_path}{map_name}.html")
+    print(f"\nMapa para '{title}' generado como .html en /generated")
+    img_data = firearm_map._to_png(5)
+    img = Image.open(io.BytesIO(img_data))
+    img.save(f"{maps_path}{map_name}.png")
+    print(f"Mapa para '{title}' generado como .png en /generated")
